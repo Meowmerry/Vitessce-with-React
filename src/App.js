10e2332e75ccdbc88b1a4fffd948f009141a4902
MeowMerry <meow3525@gmail.com>
2022-11-06 13:55:40 -0600

    get data from JS API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Vitessce } from "vitessce/dist/umd/production/index.min.js";
-// import myViewConfig from "./my-view-config.json";
+import {Vitessce} from "vitessce/dist/umd/production/index.min.js";
+import { dataConfig } from "./config";
 import "vitessce/dist/umd/production/static/css/index.css";
 import "./styles.css";
-import {viewConfig} from './my-view-config'
- 
+
 export default function App() {
+  const dataView = dataConfig()
   return (
     <div style={{ height: "800px" }}>
-      <Vitessce config={viewConfig} height={800} theme="dark" />
+      <Vitessce config={dataView} height={800} theme="dark" />
     </div>
   );
 }
